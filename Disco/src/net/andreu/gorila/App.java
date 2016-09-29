dbd1d8d56888ac6801b3a65df6280090ad4d8ef1
Andreu <Andreu@Andreu1.cendrassos2.net>
2016-09-29 19:14:30 +0200

    commit initial

--- Disco/src/net/andreu/gorila/App.java
+++ Disco/src/net/andreu/gorila/App.java
@@ -1,6 +1,8 @@
 package net.andreu.gorila;
 
 public class App {
+	
+	
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
--- Disco/src/net/andreu/gorila/Client.java
+++ Disco/src/net/andreu/gorila/Client.java
@@ -2,4 +2,34 @@ package net.andreu.gorila;
 
 public class Client {
 
+	private String nom;
+	private int edat;
+	private String sexe;
+	private int diners;
+	
+	public Client(String nom, int edat, String sexe, int diners) {
+		super();
+		this.nom = nom;
+		this.edat = edat;
+		this.sexe = sexe;
+		this.diners = diners;
+	}
+
+	public String getNom() {
+		return nom;
+	}
+
+	public int getEdat() {
+		return edat;
+	}
+
+	public String getSexe() {
+		return sexe;
+	}
+
+	public int getDiners() {
+		return diners;
+	}
+	
+	
 }
